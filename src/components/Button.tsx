6cbcecd538d0a42c858458c44aab530b7df45a4e
PrisonBreak8 <golubev.artur.1986@gmail.com>
2024-05-05 14:26:56 +0300

    modified:   src/components/Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,16 +7,19 @@ type PropsType = {
 };
 const Button = ({ children, onClick }: PropsType) => {
 	return (
-		<button
+		<button className="button-animation"
 			onClick={onClick}
 			style={{
-				padding: "10px 20px",
+				position: "relative",
+				display: "inline-block",
+				overflow: "hidden",
+				padding: "20px 30px",
 				fontSize: "1.2em",
-				borderRadius: "5px",
+				backgroundColor: " #393937",
+				color: "#03e9f4",
 				cursor: "pointer",
-				backgroundColor: "blue",
-				color: "white",
 				border: "none",
+				transition: "0.5s",
 			}}
 		>
 			{children}
